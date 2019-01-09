2b651da423d596c437d6e0a4049f38c44974ab14
divya <gopalakrishnandivya6@gmail.com>
2019-01-09 16:33:28 -0700

    commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,18 +25,25 @@ function question() {
         if (err) throw err;
         inquirer.prompt([
             {
-                type: "rawlist",
-                name: "id",
+
+                name: "list",
+                type: "list",
                 choices: function () {
-                    var choiceArray=[];
-                    for (i = 0; i < results.length; i++) {
-                        // console.log(results)
-                        choiceArray.push(results[i].product_name);
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        // console.log(results[i].item_id)
+                        choiceArray.push(results[i].item_id + " Product Name: " + results[i].product_name + " Price: " + results[i].price);
 
                     }
                     return choiceArray;
 
                 },
+                // message: "Enter the ID of the product you would like to buy?"
+
+            },
+            {
+                name: "id",
+                type: "input",
                 message: "Enter the ID of the product you would like to buy?"
             },
 
@@ -46,7 +53,47 @@ function question() {
                 message: "How many units of the product you would like to buy?"
             }
         ]).then(function (answer) {
+            // console.log(answer.id)
+            // console.log(answer.units)
+            var choiceArray;
+            
+            
+            for (var i =0; i < results.length; i++) {
 
+                if (results[i].item_id === parseInt(answer.id)) {
+                    choiceArray = results[i];
+                }
+            }
+            console.log(choiceArray.item_id)
+            if (choiceArray.stock_quantity > parseInt(answer.units)) {
+                choiceArray.stock_quantity= choiceArray.stock_quantity - parseInt(answer.units);
+                console.log(choiceArray.stock_quantity)
+                
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                          {
+                            stock_quantity: choiceArray.stock_quantity
+                          },
+                          {
+                        
+                            item_id:choiceArray.item_id
+                          }
+                        ],
+                    
+                    function (err) {
+                        if (err) throw err;
+                        console.log("Your order is placed sucessfully");
+                        question();
+                    }
+                );
+
+
+            }
+            else {
+                console.log("Insufficient quantity!");
+                question();
+            }
 
         })
     })
