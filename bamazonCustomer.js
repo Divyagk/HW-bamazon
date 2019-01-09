535611482d8bea3c0742eb3f22ad00cc2f242289
divya <gopalakrishnandivya6@gmail.com>
2019-01-09 11:51:50 -0700

    commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,53 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+
+    port: 8889,
+
+    user: "root",
+
+    password: "root",
+
+    database: "bamazon_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the question function after the connection is made to prompt the user
+    question();
+});
+
+function question() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: "rawlist",
+                name: "id",
+                choices: function () {
+                    var choices = [];
+                    for (i = 0; i < results.length; i++) {
+
+                        choices.push(results[i]);
+
+                    }
+                    return choices;
+
+                },
+                message: "Enter the ID of the product you would like to buy?"
+            },
+
+            {
+                type: "input",
+                name: "units",
+                message: "How many units of the product you would like to buy?"
+            }
+        ]) .then(function(answer) {
+
+            
+        })
+    })
+}
